e8bbf48e26435dda1d3209b1cd91aa2be18b6ef0
luodaming <775495877@qq.com>
2020-12-03 18:40:55 +0800

    feat

---
+++ day01/dataType.py
@@ -0,0 +1,13 @@
+# \ 转译 \n换行 \t表示制表符，\\ r表示默认不转译
+# i'm "ok"
+print('i\'m,\"ok\"')
+print('I\'m learning\nPython.')
+print('\\\n\\')
+# '''表示多行换行
+print('''...line1
+...line1
+...line1''')
+print(r'''hello,\n
+world''')
+# 布尔值
+print(3 > 2) #
---
+++ day01/inputAndOutput.py
@@ -0,0 +1,6 @@
+print('the quick brown fox', 'jumps over', 'the lazy dog')
+输出表达式
+print('100+200=', 100+200)
+input
+name = input()
+print('Hello,', name)
