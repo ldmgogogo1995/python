3a04537bb87abcffee8ec2b132bdf01ac71130d4
luodaming <775495877@qq.com>
2020-12-04 15:19:20 +0800

    feat variable

--- day01/dataType.py
+++ day01/dataType.py
@@ -10,4 +10,30 @@ print('''...line1
 print(r'''hello,\n
 world''')
 # 布尔值
-print(3 > 2) #
+print(3 > 2)  # T F 开头大写 逻辑运算符 直接用英文
+True and False
+# if 判断语句
+age = 18
+if age > 18:
+    print('tab1')
+else:
+    print('tab2')
+    a = 'ABC'
+    b = a
+    a = 'XYZ'
+    print(b)
+# 除法运算 精确除法，底板除法
+print(10/3)
+print(9/3)
+print(10//4)  # 向下取整
+# 求余数
+print(10 % 3)
+# print 练习
+n = 123
+f = 456.789
+s1 = 'hello,word'
+s2 = 'hello,\'Adam\''
+s3 = r'hello,"Bart"'
+s4 = r'''Hello,
+Lisa!'''
+print(n,f,s1,s2,s3,s4)
